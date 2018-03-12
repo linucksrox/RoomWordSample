4052da64887ca69f597b244c0f5de48377b21a6b
Eric Daly <edaly@hpsnet.local>
2018-03-12 14:52:10 -0400

    Create WordDao to map between methods and queries

---
+++ app/src/main/java/com/dalydays/blog/roomwordsample/WordDao.java
@@ -0,0 +1,24 @@
+package com.dalydays.blog.roomwordsample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by edaly on 3/12/2018.
+ */
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    List<Word> getAllWords();
+}
